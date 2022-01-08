d1f612d0e55738bafd9f0b80f25832b8d31656d0
lukaszniewin <lukaszniewinski1998@gmail.com>
2022-01-08 12:21:37 +0100

    update
    Add button.js -> components
    change function App -> App.js
    
    Co-Authored-By: MichalGrzelec <97067161+MichalGrzelec@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React, { useState } from 'react';
 import './App.css';
 import Zamowienie from './components/zamowienie';
 import Skomponuj from './components/skomponuj';
+import Button from './components/button';
 
-function App() {
+
+const App = () => {
+
+  const [Zart, setZart] = React.useState("");
+  
+  const fetchApi = () => {
+    fetch("https://sv443.net/jokeapi/v2/joke/Programming?type=single")
+    .then((res)=> res.json())
+    .then((data) => setZart(data.joke));
+  };
+  
 
 const [salatka, setSalatka] = useState(null);
 
@@ -21,10 +32,12 @@ const otrzymajSalatke = salatka => {
       <Skomponuj otrzymajSalatke={otrzymajSalatke} />
       <Zamowienie nowaSalatka={salatka}/>
       </div>
-     
+      <div>{Zart === "" ? <Button callApi={fetchApi} /> : 
+      <p>{Zart}</p>
+      }</div>
 
     </div>
   );
-}
 
+    }
 export default App;
---
+++ src/components/button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Button = (props) =>
+{
+    return <button onClick={props.callApi}>
+     Żart na dziś    
+    </button>
+}
+
+export default Button;
